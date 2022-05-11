6b1a97832ef01fb4dafa70b32db675613f3dc002
Terry <terrybmochire@gmail.com>
2022-05-12 01:13:53 +0300

    Write sql queries for Foodtype dao and test them.

---
+++ src/test/java/dao/Sql2oFoodtypeDaoTest.java
@@ -0,0 +1,69 @@
+package dao;
+
+import models.Foodtype;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Sql2oFoodtypeDaoTest {
+    String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+    Sql2o sql2o = new Sql2o(connectionString, "", "");
+    private Sql2oFoodtypeDao foodtypeDao = new Sql2oFoodtypeDao(sql2o);
+    private Sql2oRestaurantDao restaurantDao =new Sql2oRestaurantDao(sql2o);
+    private Connection conn;
+
+    @Before
+    void setUp() {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        restaurantDao = new Sql2oRestaurantDao(sql2o);
+        foodtypeDao = new Sql2oFoodtypeDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @After
+    void tearDown() {
+        conn.close();
+    }
+
+    public Foodtype setupNewFoodtype(){
+        return new Foodtype("Sushi");
+    }
+
+    @Test
+    void addFoodSetsId() throws Exception {
+        Foodtype testFoodtype = setupNewFoodtype();
+        int originalFoodtypeId = testFoodtype.getId();
+        foodtypeDao.add(testFoodtype);
+        assertNotEquals(originalFoodtypeId,testFoodtype.getId());
+    }
+
+    @Test
+    void getAll_returnsAllAddedFoods() throws Exception{
+        Foodtype testfoodtype = setupNewFoodtype();
+        foodtypeDao.add(testfoodtype);
+        assertEquals(1, foodtypeDao.getAll().size());
+    }
+
+    @Test
+    void deleteById_deletesCorrectFoodType() {
+        Foodtype foodtype = setupNewFoodtype();
+        foodtypeDao.add(foodtype);
+        foodtypeDao.deleteById(foodtype.getId());
+        assertEquals(0, foodtypeDao.getAll().size());
+    }
+
+    @Test
+    void clearAll() throws Exception{
+        Foodtype testFoodtype = setupNewFoodtype();
+        Foodtype otherFoodtype = setupNewFoodtype();
+        foodtypeDao.clearAll();
+        assertEquals(0, foodtypeDao.getAll().size());
+    }
+}
